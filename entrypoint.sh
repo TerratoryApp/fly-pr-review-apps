a60514243a2a9d5ebb7c97387beee7ae8018125e
Andrew Elster <elster@terratory.co>
2023-10-31 13:25:57 -0400

    Add config to launch

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,7 +38,7 @@ fi
 if ! flyctl status --app "$app"; then
   # Backup the original config file since 'flyctl launch' messes up the [build.args] section
   cp "$config" "$config.bak"
-  flyctl launch --no-deploy --copy-config --name "$app" --image "$image" --region "$region" --org "$org" --local-only
+  flyctl launch --no-deploy --copy-config --config "$config" --name "$app" --image "$image" --region "$region" --org "$org"
   # Restore the original config file
   cp "$config.bak" "$config"
 fi
